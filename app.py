9b42e3c49aacbbea64bd680193820de05c697066
Abbashozefa <72591326+Abbashozefa@users.noreply.github.com>
2023-03-19 23:53:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,17 +19,23 @@ openai.api_key=api_key
 app=Flask(__name__)
 
 
-@app.route("/")
+@app.route("/",methods=['POST','GET'])
 def home():
-    return render_template('index.html')
+    if request.method == 'POST':
+        prompt=str(request.form['msg'])
+        response =openai.Completion.create(engine="text-davinci-003",prompt=prompt,max_tokens=2000)   
+    
+        return response['choices'][0]["text"]
+    else:
+        return render_template('index.html')
 
-@app.route("/get",methods=['POST','GET'])
-def webhook():
-    prompt=str(request.form['msg'])
-    response =openai.Completion.create(engine="text-davinci-003",prompt=prompt,max_tokens=2000)
+# @app.route("/get",methods=['POST','GET'])
+# def webhook():
+#     prompt=str(request.form['msg'])
+#     response =openai.Completion.create(engine="text-davinci-003",prompt=prompt,max_tokens=2000)
       
     
-    return response['choices'][0]["text"]
+#     return response['choices'][0]["text"]
     
     
     
